a009603d0fc8caddba1c77492e493a7c240292af
maplelinfy <maplelinfy@163.com>
2022-10-18 15:19:41 +0800

    add examples

--- read_file.py
+++ read_file.py
@@ -14,6 +14,7 @@ people.xlsx: 员工列表，分为4列：姓名,日薪,入职日期,休假日期
 输出文件说明：
 project_out.xlsx：项目信息输出，分为4列：项目名称，项目开始时间，项目结束时间，项目剩余预算
 people_out.xlsx：员工排期输出：分为4列：姓名，所在项目，进入时间，退出时间
+（可参考实例文件project_out_eg.xlsx和people_out_eg.xlsx）
 所有输出文件统一放入文件夹outputFiles
 '''
 
