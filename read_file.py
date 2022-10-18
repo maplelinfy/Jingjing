c9e2f4a27c9a83fe9687ee1665aed600e3aaf0bb
maplelinfy <maplelinfy@163.com>
2022-10-18 14:59:09 +0800

    update README.md

--- read_file.py
+++ read_file.py
@@ -5,6 +5,7 @@ blackList.txt: 周一到周五但是是休息日的日期列表
 whiteList.txt: 周六日但是是工作日的日期列表
 project.xlsx: 项目列表，分为2列：项目名称,项目总预算
 people.xlsx: 员工列表，分为4列：姓名,日薪,入职日期,休假日期(用英文逗号做分割，例如2022-01-03,2022-03-04)
+（可参考实例文件project_eg.xlsx和people_eg.xlsx）
 注意：
 1、所有文件均无表头！！！第一行即为真实数据
 2、所有文件统一放入文件夹inputFiles
@@ -76,7 +77,7 @@ def load_project_list():
     project_info = read_xlsx(project_file)
     project_list = []
     for i in range(len(project_info)):
-        pro = project(i, project_info[i][1])
+        pro = project(i, float(project_info[i][1]))
         project_list.append(pro)
         project_name.append(project_info[i][0])
     return project_list
@@ -89,7 +90,7 @@ def load_people_list():
     people_list = []
     for i in range(len(people_info)):
         leave_date = people_info[i][3].split(',')
-        peo = people(i, people_info[i][1], people_info[i][2], leave_date)
+        peo = people(i, float(people_info[i][1]), people_info[i][2], leave_date)
         people_list.append(peo)
         people_name.append(people_info[i][0])
     return people_list
